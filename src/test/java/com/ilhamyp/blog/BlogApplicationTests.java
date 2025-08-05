722945f862328214bb1d745e14b9527588f983c0
Ilham Yori P <ilhamyoripradana@gmail.com>
2025-08-05 15:01:29 +0700

    Initialize Project

---
+++ src/test/java/com/ilhamyp/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilhamyp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
